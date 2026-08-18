1dcb82374624d9be7177843dcb6bf750593ad935
Pedro lucas Pereira Ciríaco <pereiraciriacop@gmail.com>
2026-08-18 11:48:00 -0300

    Exercicio 08
    Update da resolução do exercicio 08

--- exercicio08/Main.java
+++ exercicio08/Main.java
@@ -1,7 +1,74 @@
 package exercicio08;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     static public void main(String[] args){
-        
+        ArrayList<funcionario> funcionarios = new ArrayList<>(Array.asList(
+            new gerente("Jessica", 25000),
+            new desenvolvedor("Pedro", 20000),
+            new estagiario("Jasmin", 1200)
+        ));
+        for(funcionario x : funcionarios){
+            x.calculaSalario();
+        }
+    }
+}
+
+class funcionario{
+    private String nome;
+    private double salario;
+
+    funcionario(String nome, double salario){
+        this.nome=nome;
+        this.salario=salario;
+    }
+
+    public double getSalario(){
+        return salario;
+    }
+    public void calculaSalario(){
+        System.out.printf("Salario: R$ %.2f%n", salario);
+    }
+    public void mostrarFuncionario(){
+        System.out.println(nome+":");
+    }
+}
+class gerente extends funcionario{
+    gerente(String nome, double salario){
+        super(nome, salario);
+    }
+
+    @Override
+    public void calculaSalario(){
+        double salario = getSalario()*1.2;
+        mostrarFuncionario();
+        System.out.printf("Salario: R$ %.2f%n", salario);
+    }
+}
+class desenvolvedor extends funcionario{
+    double bonus = 2000;
+
+    desenvolvedor(String nome, double salario){
+        super(nome, salario);
+    }
+
+    @Override
+    public void calculaSalario(){
+        double salario = getSalario()+bonus;
+        mostrarFuncionario();
+        System.out.printf("Salario: R$ %.2f%n", salario);
+    }
+}
+class estagiario extends funcionario{
+    estagiario(String nome, double salario){
+        super(nome, salario);
+    }
+
+    @Override
+    public void calculaSalario(){
+        double salario = getSalario();
+        mostrarFuncionario();
+        System.out.printf("Salario: R$ %.2f%n", salario);
     }
 }
